99fedbc0932c770eebddf35d95f14a0747a1c807
rosalieschipper <rosalieschipper@hotmail.com>
2019-05-06 09:33:24 +0200

    lala

--- scriptWeerAapie.js
+++ scriptWeerAapie.js
@@ -1,10 +1,18 @@
-//Deze functies/data worden direct geladen
+//--------------------------------------------------------------------
+    
     window.onload = function (){
         weerOpvragenNL('250542');
+<<<<<<< HEAD
     }
     var apiKey = '4GSkbh0bQb1LCFm7gxuOYolz1MvGr4Us';
+=======
+        datumWeergeven();
+    } 
+    var apiKey = 'tzsS7To53qb8bi9jUL8Y4ZZNCj7RVXZW';
+>>>>>>> master
 
-//Deze functie vraagt het weer aan de weer-api
+//--------------------------------------------------------------------
+    
     function weerOpvragenNL(deLocatie){
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function(){ 
@@ -19,9 +27,11 @@
         var urlLinkje = "http://dataservice.accuweather.com/currentconditions/v1/" + deLocatie + "?apikey=" + apiKey;       
         xhr.open ("GET", urlLinkje, true );
         xhr.send(); 
+        
     }
 
-//Locatie input
+//--------------------------------------------------------------------
+    
     function naamInput(){
         var inputStad = document.getElementById("locatieInput").value;
         if (inputStad == ""){
@@ -30,7 +40,20 @@
         return inputStad;
     }
 
-//Locatie Key ophalen
+//--------------------------------------------------------------------
+
+    function stadInTabelWeergeven(){
+        document.getElementById("headerStad").innerHTML = document.getElementById("locatieInput").value;  
+    }
+
+//--------------------------------------------------------------------
+    
+    function inputLeegmaken(){
+        document.getElementById("locatieInput").value = null;
+    }    
+
+//--------------------------------------------------------------------
+    
     function locatieOphalen(){
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function(){
@@ -43,7 +66,12 @@
         var urlLinkje = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + apiKey + "&q=" + naamStad;
         xhr.open ("GET", urlLinkje, true );
         xhr.send();
+        stadInTabelWeergeven();
+        inputLeegmaken();
     }
+
+    //------------------------------------------------------------------
+    
     function temperatuurValue(){
         var x = document.getElementById("temperatuurNL").innerHTML;
         var temp = parseInt(x);
@@ -54,4 +82,17 @@
         else{
             document.getElementById("Plaatje").src="boots long.png";
         };
+    }
+
+    //------------------------------------------------------------------
+    
+    function datumWeergeven(){
+        var datumVandaag = new Date();
+        var dag = datumVandaag.getDate();
+        var dag2 = ((dag < 10) ? "0" : "") + dag;
+        var maand = datumVandaag.getMonth() + 1;
+        var maand2 = ((maand < 10) ? "0" : "") + maand;
+        var jaar = datumVandaag.getYear();
+        var jaar4 = ((jaar < 1900) ? (jaar + 1900) : (jaar));
+        document.getElementById("datumdiv").innerHTML = dag2 + "-" + maand2 + "-" + jaar4;
     }
